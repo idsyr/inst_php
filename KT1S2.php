c855301f94f46b27c28e418276322d3d350863f3
idsyr <148953488+idsyr@users.noreply.github.com>
2024-02-11 19:00:04 +0500

    added: KT1S2.php

---
+++ KT1S2.php
@@ -0,0 +1,40 @@
+    <?php
+    require_once 'login.php';
+    $conn = new mysqli($hn, $un, $pw, $db);
+
+    echo <<<EOL1
+<form action="" method="GET">
+    your query: <input type="text" name="q" value=""><br>
+    <input type="submit">
+</form>
+EOL1;
+    $q = $_REQUEST['q'];
+    $result = $conn->query("select * from workers where name like '%$q%'");
+    for($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+
+    $result = '';
+    foreach($data as $elem){
+        $result .= '<tr>';
+
+        $result .= '<td>' . $elem['id'] . '</td>';
+        $result .= '<td>' . $elem['name'] . '</td>';
+        $result .= '<td>' . $elem['age'] . '</td>';
+        $result .= '<td>' . $elem['salary'] . '</td>';
+
+        $result .= '</tr>';
+    }
+
+    echo <<<EOL
+    <table>
+    <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>age</th>
+        <th>salary</th>
+    </tr>
+    EOL;
+    echo $result;
+    echo '</table>';
+
+    ?>
+
